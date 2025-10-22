b48a456e7f22ae6a4d649ceaad6c72e2107dd535
vanya-vb <vbozadzhieva29@gmail.com>
2025-10-22 19:33:08 +0300

    add basic quiz component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,14 +5,18 @@ export default function Home() {
         <section
             className="w-screen h-screen bg-[url(https://images.unsplash.com/photo-1608817618454-b0e9aef7a342?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2071))] bg-top bg-cover p-6 flex justify-center items-center"
         >
-            <div className="w-full md:w-1/2 md:h-1/2 bg-blue-50/70 rounded-2xl shadow-lg flex flex-col justify-center items-center gap-12 p-6 tracking-wider">
+            <div className="w-full md:w-1/2 md:h-1/2 bg-blue-50/70 rounded-2xl shadow-lg flex flex-col justify-center items-center gap-12 p-6">
                 <div className="text-dark-blue text-center">
-                    <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-1">Quizzical</h1>
-                    <p className="lg:text-lg tracking-wide">Challenge your mind in minutes.</p>
+                    <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-1 font-[Inter] tracking-wider">
+                        Quizzical
+                    </h1>
+                    <p className="lg:text-lg">
+                        Challenge your mind in minutes.
+                    </p>
                 </div>
                 <Link
                     to="/quiz"
-                    className="inline-block tracking-wider bg-blue text-white text-center py-2 px-10 rounded-lg cursor-pointer shadow-md"
+                    className="inline-block bg-blue text-white text-center py-2 px-10 rounded-lg cursor-pointer shadow-md tracking-wide"
                 >
                     Start quiz
                 </Link>
--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,5 +1,127 @@
 export default function Quiz() {
     return (
-        <h1>Quiz</h1>
+        <section className="relative w-screen min-h-screen bg-blue-50 p-8 flex justify-center items-center">
+            <img src="/src/assets/yellow.png" alt="" className="absolute top-0 right-0" />
+            <img src="/src/assets/blue.png" alt="" className="absolute bottom-0 left-0" />
+            <form className="w-full lg:w-3xl flex flex-col">
+
+                <div className="text-dark-blue border-b border-gray-200 pb-4 mt-4 z-1">
+                    <h2 className="text-lg font-bold mb-2">Lorem ipsum dolor sit amet consectetur?</h2>
+                    <div className="flex flex-col gap-3 md:gap-10 sm:flex-row" >
+                        <label className="inline-block text-center text-sm px-4 py-1 border border-blue rounded-xl cursor-pointer">
+                            <input type="radio" name="option" value="1" className="appearance-none absolute opacity-0" />
+                            <span>Option 1</span>
+                        </label>
+                        <label className="inline-block text-center text-sm px-4 py-1 border border-blue rounded-xl cursor-pointer">
+                            <input type="radio" name="option" value="2" className="appearance-none absolute opacity-0" />
+                            <span>Option 2</span>
+                        </label>
+                        <label className="inline-block text-center text-sm px-4 py-1 border border-blue rounded-xl cursor-pointer">
+                            <input type="radio" name="option" value="3" className="appearance-none absolute opacity-0" />
+                            <span>Option 3</span>
+                        </label>
+                        <label className="inline-block text-center text-sm px-4 py-1 border border-blue rounded-xl cursor-pointer">
+                            <input type="radio" name="option" value="4" className="appearance-none absolute opacity-0" />
+                            <span>Option 4</span>
+                        </label>
+                    </div>
+                </div>
+
+                <div className="text-dark-blue border-b border-gray-200 pb-4 mt-4">
+                    <h2 className="text-lg font-bold mb-2">Lorem ipsum dolor sit amet consectetur?</h2>
+                    <div className="flex flex-col gap-3 md:gap-10 sm:flex-row" >
+                        <label className="inline-block text-center text-sm px-4 py-1 border border-blue rounded-xl cursor-pointer">
+                            <input type="radio" name="option" value="1" className="appearance-none absolute opacity-0" />
+                            <span>Option 1</span>
+                        </label>
+                        <label className="inline-block text-center text-sm px-4 py-1 border border-blue rounded-xl cursor-pointer">
+                            <input type="radio" name="option" value="2" className="appearance-none absolute opacity-0" />
+                            <span>Option 2</span>
+                        </label>
+                        <label className="inline-block text-center text-sm px-4 py-1 border border-blue rounded-xl cursor-pointer">
+                            <input type="radio" name="option" value="3" className="appearance-none absolute opacity-0" />
+                            <span>Option 3</span>
+                        </label>
+                        <label className="inline-block text-center text-sm px-4 py-1 border border-blue rounded-xl cursor-pointer">
+                            <input type="radio" name="option" value="4" className="appearance-none absolute opacity-0" />
+                            <span>Option 4</span>
+                        </label>
+                    </div>
+                </div>
+
+                <div className="text-dark-blue border-b border-gray-200 pb-4 mt-4">
+                    <h2 className="text-lg font-bold mb-2">Lorem ipsum dolor sit amet consectetur?</h2>
+                    <div className="flex flex-col gap-3 md:gap-10 sm:flex-row" >
+                        <label className="inline-block text-center text-sm px-4 py-1 border border-blue rounded-xl cursor-pointer">
+                            <input type="radio" name="option" value="1" className="appearance-none absolute opacity-0" />
+                            <span>Option 1</span>
+                        </label>
+                        <label className="inline-block text-center text-sm px-4 py-1 border border-blue rounded-xl cursor-pointer">
+                            <input type="radio" name="option" value="2" className="appearance-none absolute opacity-0" />
+                            <span>Option 2</span>
+                        </label>
+                        <label className="inline-block text-center text-sm px-4 py-1 border border-blue rounded-xl cursor-pointer">
+                            <input type="radio" name="option" value="3" className="appearance-none absolute opacity-0" />
+                            <span>Option 3</span>
+                        </label>
+                        <label className="inline-block text-center text-sm px-4 py-1 border border-blue rounded-xl cursor-pointer">
+                            <input type="radio" name="option" value="4" className="appearance-none absolute opacity-0" />
+                            <span>Option 4</span>
+                        </label>
+                    </div>
+                </div>
+
+                <div className="text-dark-blue border-b border-gray-200 pb-4 mt-4">
+                    <h2 className="text-lg font-bold mb-2">Lorem ipsum dolor sit amet consectetur?</h2>
+                    <div className="flex flex-col gap-3 md:gap-10 sm:flex-row" >
+                        <label className="inline-block text-center text-sm px-4 py-1 border border-blue rounded-xl cursor-pointer">
+                            <input type="radio" name="option" value="1" className="appearance-none absolute opacity-0" />
+                            <span>Option 1</span>
+                        </label>
+                        <label className="inline-block text-center text-sm px-4 py-1 border border-blue rounded-xl cursor-pointer">
+                            <input type="radio" name="option" value="2" className="appearance-none absolute opacity-0" />
+                            <span>Option 2</span>
+                        </label>
+                        <label className="inline-block text-center text-sm px-4 py-1 border border-blue rounded-xl cursor-pointer">
+                            <input type="radio" name="option" value="3" className="appearance-none absolute opacity-0" />
+                            <span>Option 3</span>
+                        </label>
+                        <label className="inline-block text-center text-sm px-4 py-1 border border-blue rounded-xl cursor-pointer">
+                            <input type="radio" name="option" value="4" className="appearance-none absolute opacity-0" />
+                            <span>Option 4</span>
+                        </label>
+                    </div>
+                </div>
+
+                <div className="text-dark-blue border-b border-gray-200 pb-4 mt-4 z-1">
+                    <h2 className="text-lg font-bold mb-2">Lorem ipsum dolor sit amet consectetur?</h2>
+                    <div className="flex flex-col gap-3 md:gap-10 sm:flex-row" >
+                        <label className="inline-block text-center text-sm px-4 py-1 border border-blue rounded-xl cursor-pointer">
+                            <input type="radio" name="option" value="1" className="appearance-none absolute opacity-0" />
+                            <span>Option 1</span>
+                        </label>
+                        <label className="inline-block text-center text-sm px-4 py-1 border border-blue rounded-xl cursor-pointer">
+                            <input type="radio" name="option" value="2" className="appearance-none absolute opacity-0" />
+                            <span>Option 2</span>
+                        </label>
+                        <label className="inline-block text-center text-sm px-4 py-1 border border-blue rounded-xl cursor-pointer">
+                            <input type="radio" name="option" value="3" className="appearance-none absolute opacity-0" />
+                            <span>Option 3</span>
+                        </label>
+                        <label className="inline-block text-center text-sm px-4 py-1 border border-blue rounded-xl cursor-pointer">
+                            <input type="radio" name="option" value="4" className="appearance-none absolute opacity-0" />
+                            <span>Option 4</span>
+                        </label>
+                    </div>
+                </div>
+
+                <button
+                    className="bg-blue text-white self-center text-center py-2 px-10 rounded-lg cursor-pointer shadow-md tracking-wide mt-8 z-1"
+                >
+                    Check answers
+                </button>
+
+            </form>
+        </section>
     );
 };
